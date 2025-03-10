5ec64b826d9db60e7f9e7d1ccfddd5e594503319
SubhashPrasad23 <SubhashPrasad027@gmail.com>
2025-03-10 14:17:11 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import MusicContainer from './components/musicContainer'
+import MusicContainer from './components/MusicContainer'
 
 const App = () => {
   return (
     <div className='place-content-center bg-gradient-to-t from-gray-800 via-gray-500 to-gray-800 min-h-screen w-full'>
-    <MusicContainer />
+    <MusicContainer/>
     </div>
   )
 }
